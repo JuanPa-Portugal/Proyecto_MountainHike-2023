bb65d19df7fe90d4647655bce9e5991508dda25e
Claudio-Olivera <claudioolivera753@gmail.com>
2023-04-29 10:25:38 -0300

    Add AppRoutingModule in shared module

--- Frontend-Angular/MountainHike/src/app/shared/shared.module.ts
+++ Frontend-Angular/MountainHike/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -11,7 +12,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
     HeaderComponent,
