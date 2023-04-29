982b1ea998ff2c423ea0d5a658197174c94168f9
SoledadPeralta <solperalta078@gmail.com>
2023-04-29 19:01:44 -0300

    Agregué el módulo Auth, el componente register y corregí una referencia a una fuente que daba error

--- Frontend-Angular/MountainHike/src/app/app.module.ts
+++ Frontend-Angular/MountainHike/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
-    SharedModule
+    SharedModule,
+    AuthModule
     ],
   providers: [],
   bootstrap: [AppComponent]
