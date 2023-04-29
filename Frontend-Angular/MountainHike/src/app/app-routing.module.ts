7935dd2b5f3fc4af561cfde576e7f77843974dc5
Claudio-Olivera <claudioolivera753@gmail.com>
2023-04-29 10:38:34 -0300

    Add import-export in app-routing.module

--- Frontend-Angular/MountainHike/src/app/app-routing.module.ts
+++ Frontend-Angular/MountainHike/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
