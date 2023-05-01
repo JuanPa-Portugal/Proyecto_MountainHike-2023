c93cac3edc7a8f861b2a9cef8588a1a1b79fa34d
SoledadPeralta <solperalta078@gmail.com>
2023-05-01 14:03:54 -0300

    Agrego el ruteo del blog

--- Frontend-Angular/MountainHike/src/app/app-routing.module.ts
+++ Frontend-Angular/MountainHike/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'register', component: RegisterComponent} 
+  { path: 'register', component: RegisterComponent} ,
+  { path: 'blog', component: BlogsComponent}
   /* Aqui ir agregando las demás rutas EJ: {path:'nosotros', component:NosotrosComponent}*/
 ];
 
