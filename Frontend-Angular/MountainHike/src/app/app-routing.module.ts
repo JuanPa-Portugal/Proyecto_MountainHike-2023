bb7c0d5ae631c3f4df9c81e4794938f38ad34131
ezequiel falzoi <ezequielfalzoi@gmail.com>
2023-05-01 17:34:24 -0300

    agrego inicio secion y admin en rama feature

--- Frontend-Angular/MountainHike/src/app/app-routing.module.ts
+++ Frontend-Angular/MountainHike/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { InicioAdminComponent } from './auth/inicio-admin/inicio-admin.component';
+import { IniciaSecionComponent } from './auth/inicia-secion/inicia-secion.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'register', component: RegisterComponent} 
+  { path: 'register', component: RegisterComponent},
+  { path: 'userLogin', component: IniciaSecionComponent},
+  { path: 'adminLogin', component: InicioAdminComponent}
   /* Aqui ir agregando las demás rutas EJ: {path:'nosotros', component:NosotrosComponent}*/
 ];
 
