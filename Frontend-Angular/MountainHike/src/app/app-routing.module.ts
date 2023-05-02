8ce7066255a3a19ce19d328b813f4bad12e28836
KevinPaez <kevin.agustin-2000@outlook.es>
2023-05-02 16:11:52 -0300

    borro node, y agrego routing

--- Frontend-Angular/MountainHike/src/app/app-routing.module.ts
+++ Frontend-Angular/MountainHike/src/app/app-routing.module.ts
@@ -2,28 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
-<<<<<<< HEAD
-
-=======
 import { InicioAdminComponent } from './auth/inicio-admin/inicio-admin.component';
 import { IniciaSesionComponent } from './auth/inicia-sesion/inicia-sesion.component';
 import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
 import { UserPanelComponent } from './pages/user-panel/user-panel.component';
->>>>>>> 739daafa82fa34753fb5bd96e4b3b339c29ca950
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'register', component: RegisterComponent},
-<<<<<<< HEAD
-  
-=======
   { path: 'userLogin', component: IniciaSesionComponent},
   { path: 'adminLogin', component: InicioAdminComponent},
-  { path: 'admin-panel', component: AdminPanelComponent },
+  { path: 'admin-panel', component: AdminPanelComponent},
   { path: 'user-panel', component: UserPanelComponent},
   { path:'nosotros', component: NosotrosComponent}
->>>>>>> 739daafa82fa34753fb5bd96e4b3b339c29ca950
   /* Aqui ir agregando las demás rutas EJ: {path:'nosotros', component:NosotrosComponent}*/
 ];
 
