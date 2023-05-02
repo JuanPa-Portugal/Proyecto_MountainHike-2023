9084d5a4db276731bb2da588427a5c0540d967d9
KevinPaez <kevin.agustin-2000@outlook.es>
2023-05-02 14:00:38 -0300

    Agregando pagina Nostoros a Angular

--- Frontend-Angular/MountainHike/src/app/app-routing.module.ts
+++ Frontend-Angular/MountainHike/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'register', component: RegisterComponent} 
+  { path: 'register', component: RegisterComponent},
+  { path:'nosotros', component: NosotrosComponent}
   /* Aqui ir agregando las demás rutas EJ: {path:'nosotros', component:NosotrosComponent}*/
 ];
 
