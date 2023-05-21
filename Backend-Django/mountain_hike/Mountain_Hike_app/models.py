ec917ec54570006b5e22a3b9e82528ad54958f79
KevinPaez <kevin.agustin-2000@outlook.es>
2023-05-21 20:19:56 -0300

    algo

--- Backend-Django/mountain_hike/Mountain_Hike_app/models.py
+++ Backend-Django/mountain_hike/Mountain_Hike_app/models.py
@@ -107,45 +107,7 @@ class Recorridos(models.Model):
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-class Guias(models.Model):
+    class Guias(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
