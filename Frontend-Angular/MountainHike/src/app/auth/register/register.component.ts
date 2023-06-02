40e9200de42701ac74d664a91b2ac3905041eb6a
Florencia Morisse <florenciamorisse@gmail.com>
2023-06-02 09:28:13 -0300

    hice el formulario para registrarse faltan algunas validaciones

--- Frontend-Angular/MountainHike/src/app/auth/register/register.component.ts
+++ Frontend-Angular/MountainHike/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-}
+  registroForm=this.formBuilder.group({
+    nombre:['Florencia', Validators.required],
+    apellido:['Morisse', Validators.required],
+    email:['florencia@gmail.com',[Validators.required, Validators.email]],
+    password:['1234', [Validators.required, Validators.minLength(8)]],
+    cb:[false, Validators.requiredTrue]
+  })
+
+  constructor(private formBuilder:FormBuilder, private router:Router){}
+
+  get nombre(){
+    return this.registroForm.controls.nombre
+  }
+  get apellido(){
+    return this.registroForm.controls.apellido
+  }
+  get email(){
+    return this.registroForm.controls.email
+  }
+  get password(){
+    return this.registroForm.controls.password
+  }
+  get cb(){
+    return this.registroForm.controls.cb
+  }
+
+  registro(){
+
+    if (this.registroForm.valid){
+      this.router.navigateByUrl('/recorridos');
+      //me redirige hacia recorridos, me tiene que redirigir al dashboard que hay que hacer
+    }
+    else{
+      alert('error al ingresar los datos')
+    }
+  }
+  }
+
+
