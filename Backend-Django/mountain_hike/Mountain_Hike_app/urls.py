4d79e87192545c41429bf42bbdbbb12daef3b802
SoledadPeralta <solperalta078@gmail.com>
2023-06-02 20:50:03 -0300

    Corrijo errores en urls para los endpoints de blog, recorrido, guias, etc.

--- Backend-Django/mountain_hike/Mountain_Hike_app/urls.py
+++ Backend-Django/mountain_hike/Mountain_Hike_app/urls.py
@@ -5,30 +5,12 @@ from Mountain_Hike_app import views
 
 router= routers.DefaultRouter()
 router.register(r'Blog',views.BlogViewSet)
-#----
-urlpatterns = [
-     path('', include(router.urls)),
-]
-
-
-router= routers.DefaultRouter()
 router.register(r'Reserva',views.ReservaViewSet)
-#----
-urlpatterns = [
-     path('', include(router.urls)),
-]
-
-router= routers.DefaultRouter()
 router.register(r'Recorridos',views.RecorridosViewSet)
-#----
-urlpatterns = [
-     path('', include(router.urls)),
-]
-
-
-router= routers.DefaultRouter()
 router.register(r'Guias',views.GuiasViewSet)
+
 #----
 urlpatterns = [
      path('', include(router.urls)),
 ]
+
--- Backend-Django/mountain_hike/mountain_hike/urls.py
+++ Backend-Django/mountain_hike/mountain_hike/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1', include('Mountain_Hike_app.urls')),
+    path('api/v1/', include('Mountain_Hike_app.urls')),
    
 ]
  
