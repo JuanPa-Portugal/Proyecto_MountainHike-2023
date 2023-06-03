03a1ce98611a2a651c30ec85b8f135f293582a65
SoledadPeralta <solperalta078@gmail.com>
2023-06-03 15:28:51 -0300

    Completada la guia API Rest para la autenticacion.
    Visitando http://127.0.0.1:8000/api/v1/auth/login/ y enviando {"username":"admin@admin.com", "password":"admin321"} puede verificarse funcionamiento correcto
    Para desloguearse, http://127.0.0.1:8000/api/v1/auth/logout y enviar sin datos.

--- Backend-Django/mountain_hike/Mountain_Hike_app/urls.py
+++ Backend-Django/mountain_hike/Mountain_Hike_app/urls.py
@@ -3,6 +3,8 @@ from rest_framework import routers
 #from Blog.views import BlogViewSet
 from Mountain_Hike_app import views
 
+from .views import LoginView, LogoutView
+
 router= routers.DefaultRouter()
 router.register(r'Blog',views.BlogViewSet)
 router.register(r'Reserva',views.ReservaViewSet)
@@ -11,6 +13,8 @@ router.register(r'Guias',views.GuiasViewSet)
 
 #----
 urlpatterns = [
+     path('auth/login/',LoginView.as_view(),name='auth_login'),
+     path('auth/logout/',LogoutView.as_view(), name='auth_logout'),
      path('', include(router.urls)),
 ]
 
--- Backend-Django/mountain_hike/Mountain_Hike_app/views.py
+++ Backend-Django/mountain_hike/Mountain_Hike_app/views.py
@@ -11,19 +11,45 @@ from .models import Reserva
 from .models import Recorridos
 from .models import Guias
 
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+        user = authenticate(email=email, password=password)
+
+        if user:
+            login (request, user)
+            return Response(
+                status =status.HTTP_200_OK)
+        
+        return Response(
+            status=status.HTTP_401_UNAUTHORIZED)
+        
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
+    
+
 class BlogViewSet(viewsets.ModelViewSet):
- queryset=Blog.objects.all()
- serializer_class= BlogSerializer
+    queryset=Blog.objects.all()
+    serializer_class= BlogSerializer
 
 
 class ReservaViewSet(viewsets.ModelViewSet):
- queryset=Reserva.objects.all()
- serializer_class= ReservaSerializer
+    queryset=Reserva.objects.all()
+    serializer_class= ReservaSerializer
 
 class RecorridosViewSet(viewsets.ModelViewSet):
- queryset=Recorridos.objects.all()
- serializer_class= RecorridosSerializer
+    queryset=Recorridos.objects.all()
+    serializer_class= RecorridosSerializer
 
 class GuiasViewSet(viewsets.ModelViewSet):
- queryset=Guias.objects.all()
- serializer_class= GuiasSerializer
+    queryset=Guias.objects.all()
+    serializer_class= GuiasSerializer
