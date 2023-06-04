5b62d99f7d4c6313876467821a83ab58c45224f2
Florencia Morisse <florenciamorisse@gmail.com>
2023-06-04 18:48:37 -0300

    borré una carpeta node/ hice pag 404

---
+++ Frontend-Angular/MountainHike/src/app/pages/pagina404/pagina404.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina404',
+  templateUrl: './pagina404.component.html',
+  styleUrls: ['./pagina404.component.css']
+})
+export class Pagina404Component {
+
+}
