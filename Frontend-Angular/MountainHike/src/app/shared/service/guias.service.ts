457d813143fb4c7e253b34370aac76b72bc65426
ezequiel falzoi <ezequielfalzoi@gmail.com>
2023-06-04 12:19:21 -0300

    agrego delete, patch y post alguias

--- Frontend-Angular/MountainHike/src/app/shared/service/guias.service.ts
+++ Frontend-Angular/MountainHike/src/app/shared/service/guias.service.ts
@@ -14,4 +14,16 @@ export class GuiasService {
   getGuias(): Observable<Guias[]>{
     return this.http.get<Guias[]>(this.apiGuias);
   }
+  deleteGuias(id:any):Observable<any>{
+    return this.http.delete(`${this.apiGuias}/${id}`);
+  }
+
+  patchGuias(arr: Guias, id:any):Observable<any>{
+    console.log(arr, id)
+    return this.http.patch(`${this.apiGuias}/${id}`,arr)
+  }
+
+  postGuias(arr: Guias):Observable<any>{
+      return this.http.post(`${this.apiGuias}`,arr)
+  }
 }
