57d333968a5fe038d75efd6c49d190208dfe648e
Florencia Morisse <florenciamorisse@gmail.com>
2023-06-12 10:45:57 -0300

    cambié idioma al back

--- Backend-Django/mountain_hike/mountain_hike/settings.py
+++ Backend-Django/mountain_hike/mountain_hike/settings.py
@@ -116,7 +116,7 @@ AUTH_USER_MODEL = "Mountain_Hike_app.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-eu'
 
 TIME_ZONE = 'UTC'
 
